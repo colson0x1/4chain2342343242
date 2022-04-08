2e7c384a65651d4b3a0e3d4aeab2ab64dfa54650
Colson <colson@Colsons-MacBook-Pro.local>
2022-04-08 11:11:23 +0545

    initialized spring boot

---
+++ src/test/java/com/colson0x1/chain/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colson0x1.chain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
